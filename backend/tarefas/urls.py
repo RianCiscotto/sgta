e7a4a84b52b7014aca55b9deb0c56c3a14fa4ed1
Rian <rianciscotto49@gmail.com>
2026-03-23 21:15:09 -0300

    Criação de rotas, tarefas, etc

---
+++ backend/tarefas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import listar_tarefas
+
+urlpatterns = [
+    path('tarefas/', listar_tarefas),
+    
+    
+]
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Tarefa
+from django.http import JsonResponse
+
+
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().values()
+    return JsonResponse(list[any](tarefas), safe=False)
+
