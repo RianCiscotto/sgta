470a6a16633e177e87ba713d512cdc7a14b61901
Rian <Rianciscotto49@gmail.com>
2026-06-01 20:21:37 -0300

    Projeto Django com Docker configurado

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Aguardando banco..."
+
 python manage.py migrate
 
-python manage.py runserver 0.0.0.0:8000
+echo "Iniciando servidor..."
+exec python manage.py runserver 0.0.0.0:8000
